fcf99ce8695021767c6d849c126415a644769690
Shagun Bose <32908915+shagunBose@users.noreply.github.com>
2018-02-12 21:57:40 -0500

    Math and Logic
    Edited Math and Logic for the game!
    Things work now
    
    Stuff to add - tile sliding animation, sound effects, popping new tile

--- src/Game.java
+++ src/Game.java
@@ -7,6 +7,7 @@ import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.ArrayList;
@@ -16,14 +17,14 @@ import java.util.Random;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
-public class Game extends JPanel implements KeyListener, ActionListener{
+public class Game extends JPanel{
 	public static final int Width = 470;
 	public static final int Height = 600;
 
 	public final int target = 2048; //target variable to reach 
-	static int biggestTile; //highest tile reached
+	static int biggestTile = 2; //highest tile reached
 	static int score; //current score
-	static int highscore;
+	static int highscore = 0;
 
 	public Color gridColor = new Color(192, 165, 140); //colour of lines of grid
 	public Color backgroundColor = new Color(210, 192, 173); //colour of background of grid
@@ -36,39 +37,99 @@ public class Game extends JPanel implements KeyListener, ActionListener{
 	public boolean lostGame = false; //to check status of game - win or loose. 
 	public boolean wonGame = false;
 	
-	JButton start;
-	private int emptySpaces = 0;
+	public boolean movesR = true;
+	public boolean movesL = true;
+	public boolean movesU = true;
+	public boolean movesD = true;
 	
-			
+	public int moves = 0;
+	
+	JButton start;
+	
+	KeyAdapter key;
+	
+	
 	public Game() {
 		setLayout(null);
 		setPreferredSize(new Dimension(470, 600));
 		setFocusable(true); //allows keyboard input 
-		addKeyListener(this);
 		
-		start = new JButton("START");
-		start.setBounds(342, 100, 80, 30);
-		start.addActionListener(this);
-		start.setBackground(backgroundColor);
-		start.setFocusPainted(false);
-		this.add(start);
-		
+		addKeyListener(key = new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {	
+				if(e.getKeyCode() == KeyEvent.VK_SPACE){
+					start();
+					System.out.println("Game Started");
+				}
+				
+				if(isRunning) {
+						String s = "";
+						switch(e.getKeyCode()) {
+						case KeyEvent.VK_RIGHT: 
+							moveRight();
+							s += "Right Key Pressed";
+							break;
+						case KeyEvent.VK_LEFT: 
+							moveLeft();
+							s += "Left Key Pressed";
+							break;
+						case KeyEvent.VK_UP: 
+							moveUp();
+							s += "Up Key Pressed";
+							break;
+						case KeyEvent.VK_DOWN: 
+							moveDown();
+							s += "Down Key Pressed";
+							break;
+						case KeyEvent.VK_SPACE:
+							break;
+						default: s += "Invalid Key Pressed >>> " + e.getKeyChar();
+						}
+						
+						System.out.println(s + "\nMoves: " + moves + " Highest Tile on Board: " + biggestTile + "\n");
+						for(int i = 0; i <4; i ++) {
+							for(int j = 0; j <4; j++) {
+								tiles[i][j].clearMerged();
+							}
+						}
+						
+						if(!movesR && !movesL && !movesU && !movesD) {
+							isRunning = false;
+							lostGame = true;
+						}
+					} 
+					
+					repaint();
+
+				}		
+		});
+
 		//render();
 	}
 	
 	public void start() {
-			
-		if(!isRunning) {
-			isRunning = true;
-			start.setText("REFRESH");
-		}
+		//set game board values
 			tiles = new Tile[4][4];
+			for(int i = 0; i < 4; i++) {
+				for (int j = 0; j < 4; j++){
+					tiles[i][j] = new Tile(0);
+					}
+						}
+			//set score values
+			if(score> highscore) {
+				highscore = score;
+			}
 			score = 0;
 			biggestTile = 0;
+			moves = 0;
+			//set game state 
+			isRunning = true;
+			movesR = true; movesL = true; movesU = true; movesD = true;
+			//add random tiles
 			addRandomTile();
 			addRandomTile();
+			//render game
 			repaint();
-		
 	}
 	
 	@Override
@@ -93,17 +154,16 @@ public class Game extends JPanel implements KeyListener, ActionListener{
 			g.fillRect(274, 13, 65, 60); //score
 			g.setColor(gridColor);
 			g.fillRect(274+65, 13, 120, 60); //high score
-			g.fillRoundRect(277+65, 100, 80, 30, 15, 15); //start
 			g.setFont(new Font("ArialBlack", Font.BOLD, 18));
 			g.setColor(Color.white);
 			g.drawString("Score", 280, 30);
 			g.drawString("High Score", 350, 30);
-			g.drawString(""+score, 300, 57);
+			g.drawString(""+score, 280, 57);
 			g.drawString(""+highscore, 400, 57);
-			g.drawString("START", 277+75, 123);
-			g.setColor(new Color(49, 38, 27));
+			
+			g.setColor(new Color(49, 38, 27)); //instructions
 			g.setFont(new Font("ArialBlack", Font.BOLD, 14));
-			g.drawString("Join the numbers and get to the 2048 tile!", 10, 120);
+			g.drawString("Join the numbers and get to the 2048 tile!", 10, 130);
 			
 			
 			
@@ -115,10 +175,10 @@ public class Game extends JPanel implements KeyListener, ActionListener{
 					for(int i = 0; i <4; i++) {
 						for (int j = 0; j < 4; j++) {
 							
-							if(tiles[i][j] == null) {
+							if(tiles[i][j].getValue() == 0) {
 								g.setColor(backgroundColor);
-								int x = 20 + 10*i + Tile.tileWidth*i;
-								int y = 170 + 10*j + Tile.tileHeight*j;
+								int x = 20 + 10*j + Tile.tileWidth*j;
+								int y = 170 + 10*i + Tile.tileHeight*i;
 								g.fillRoundRect(x, y, 100, 100, 15, 15);
 							} else {
 								drawTile(g, i, j);
@@ -129,7 +189,8 @@ public class Game extends JPanel implements KeyListener, ActionListener{
 				}else if(!isRunning && lostGame) {
 					g.setColor(Color.BLACK);
 					g.setFont(new Font("FunSized", Font.BOLD, 20));
-					g.drawString("Game Over", 155, 365);
+					g.drawString("No More Moves!", 145, 365);
+					g.drawString("Score: " + score, 155, 390);
 				}else if(!isRunning && wonGame) {
 					g.setColor(Color.BLACK);
 					g.setFont(new Font("FunSized", Font.BOLD, 20));
@@ -137,7 +198,7 @@ public class Game extends JPanel implements KeyListener, ActionListener{
 				}else {
 					g.setColor(Color.BLACK);
 					g.setFont(new Font("FunSized", Font.BOLD, 20));
-					g.drawString("Click START", 155, 365);
+					g.drawString("Press SPACE to START", 95, 385);
 				}
 				
 				
@@ -145,9 +206,10 @@ public class Game extends JPanel implements KeyListener, ActionListener{
 	
 	public void drawTile(Graphics2D g, int row, int col) {
 		int value = tiles[row][col].getValue();
+		
 		g.setColor(Tile.setBGColor(value));
-		int x = 20 +10*row + 100*row;
-		int y = 170 +10*col + 100*col;
+		int x = 20 +10*col + 100*col;
+		int y = 170 +10*row + 100*row;
 		g.fillRoundRect(x, y, 100, 100, 15, 15);
 		
 		g.setFont(new Font("FunSized", Font.PLAIN, 35));
@@ -159,39 +221,30 @@ public class Game extends JPanel implements KeyListener, ActionListener{
 	}
 	
 	public void addRandomTile() {
-		
-		ArrayList<Integer> r = new ArrayList<Integer>();
-		ArrayList<Integer> c = new ArrayList<Integer>();
+			ArrayList<Integer> r = new ArrayList<Integer>();
+			ArrayList<Integer> c = new ArrayList<Integer>();
 
-		List<Tile> availableSpace = new ArrayList<Tile>();
-		for(int i = 0; i<4; i++) {
-			for(int j = 0; j <4; j++) {
-				if(tiles[i][j] == null) {
-					availableSpace.add(tiles[i][j]);
-					r.add(i);
-					c.add(j);
-				} else {
-					continue;
+			List<Tile> availableSpace = new ArrayList<Tile>();
+			for(int i = 0; i<4; i++) {
+				for(int j = 0; j <4; j++) {
+					if(tiles[i][j].getValue() == 0) {
+						availableSpace.add(tiles[i][j]);
+						r.add(i);
+						c.add(j);
+					} else {
+						continue;
+					}
 				}
-			}
-		}
-		
-		if(availableSpace.isEmpty()) {
-			MovesAvailable = false;
-			isRunning = false;
-			repaint();
-		} else {
-			int index = rand.nextInt(availableSpace.size());
-			tiles[r.get(index)][c.get(index)] = new Tile(2);
-		}
-	
+			} 
+				int index = rand.nextInt(availableSpace.size());
+				tiles[r.get(index)][c.get(index)] = new Tile(2);
 	}
 	
 	public int emptySpaces() {
-		emptySpaces = 0;
+		int emptySpaces = 0;
 		for(int i = 0; i<4; i++) {
 			for(int j = 0; j <4; j++) {
-				if(tiles[i][j] == null) {
+				if(tiles[i][j].getValue() == 0) {
 					emptySpaces ++;
 				} else {
 					continue;
@@ -200,84 +253,180 @@ public class Game extends JPanel implements KeyListener, ActionListener{
 		}
 		return emptySpaces;
 	}
+
 	
-	public boolean isAvailable() {
-		if(emptySpaces == 0) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-
-
-	@Override
-	public void keyTyped(KeyEvent e) {}
-
-	@Override
-	public void keyPressed(KeyEvent e) {
-		if(!isAvailable()) {
-			lostGame = true;
-		}
-		
-		if(!wonGame && !lostGame && isRunning) {
-			switch(e.getKeyCode()) {
-			case KeyEvent.VK_RIGHT: 
-				moveRight();
-				addRandomTile();
-				break;
-			case KeyEvent.VK_LEFT: 
-				moveLeft();
-				break;
-			case KeyEvent.VK_UP: 
-				moveUp();
-				break;
-			case KeyEvent.VK_DOWN: 
-				moveDown();
-				break;
-			}	
-		}
-		repaint();
-	}
-
-	@Override
-	public void keyReleased(KeyEvent e) {}
-	
-	public void moveRight() {
-		System.out.println("move right");
-		for(int i = 0; i < 4; i++) {
-			for (int j = 2; j <=0; j--) {
-				for(int k = j; k < 3; k++) {
-					if(tiles[i][k+1] == null) {
-						tiles[i][k+1].setValue(tiles[i][k].getValue());
-						tiles[i][k] = null;
-					} else if (tiles[i][k+1].getValue() == tiles[i][k].getValue()) {
-						tiles[i][k+1].setValue(tiles[i][k].getValue()*2);
-						tiles[i][k] = null;
-					}
+	public void FindbiggestTile() {
+		for(int i = 0; i<4; i++) {
+			for(int j = 0; j<4; j++) {
+				int val = tiles[i][j].getValue();
+				if(val == 0) {
+					continue;
+				}
+				
+				if(val > biggestTile) {
+					biggestTile = val;
 				}
 			}
 		}
 	}
 	
-	public void moveLeft() {
 	
+	public void moveRight() {
+		int valid = 0;
+		for(int i = 0; i < 4; i++) {
+			for (int j = 2; j >=0; j--) {
+				for(int k = j; k < 3; k++) {
+					int val = tiles[i][k].getValue();
+					if(tiles[i][k].getValue()==0) {
+						continue;
+					} else {
+						if((tiles[i][k+1].getValue() == 0)) {
+							tiles[i][k+1].setValue(val);
+							//tiles[i][k+1].setValue(tiles[i][k].getValue());
+							tiles[i][k].setValue(0);
+							valid++;
+						
+						} else if (tiles[i][k+1].getValue() == val) {
+							if(tiles[i][k+1].mergeable()&& tiles[i][k].mergeable()) {
+								tiles[i][k+1].setValue(val*2);
+								score += val*2;
+								tiles[i][k+1].Merged();
+								tiles[i][k].setValue(0);
+								valid++;
+							}
+			
+						}
+					}
+					
+				}
+			}
+		}
+		if(valid > 0) {
+			addRandomTile();
+			System.out.println("Valid Move");
+			moves++;
+		} else {
+			System.out.println("Invalid Move");
+			movesR = false;
+		}
+		FindbiggestTile();
+	}
+	
+	public void moveLeft() {
+		int valid = 0;
+		for(int i = 0; i < 4; i++) {
+			for (int j=1; j < 4; j++) {
+				for(int k = j; k > 0; k--) {
+					int val = tiles[i][k].getValue();
+					if(tiles[i][k].getValue()==0) {
+						continue;
+					} else {
+						if((tiles[i][k-1].getValue() == 0)) {
+							tiles[i][k-1].setValue(val);
+							tiles[i][k].setValue(0);
+							valid++;
+						} else if (tiles[i][k-1].getValue() == val) {
+							if(tiles[i][k-1].mergeable()&& tiles[i][k].mergeable()) {
+								tiles[i][k-1].setValue(val*2);
+								score += val*2;
+								tiles[i][k-1].Merged();
+								tiles[i][k].setValue(0);
+								valid++;
+							}
+							
+							
+						}
+					}
+					
+				}
+			}
+		}
+		if(valid > 0) {
+			addRandomTile();
+			System.out.println("Valid Move");
+			moves++;
+		}else {
+			System.out.println("Invalid Move");
+			movesL = false;
+		}
+		FindbiggestTile();
 	}
 	
 	public void moveUp() {
+		int valid = 0;
+		for(int j = 0; j < 4; j++) {
+			for (int i=1; i < 4; i++) {
+				for(int k = i; k > 0; k--) {
+					int val = tiles[k][j].getValue();
+					if(tiles[k][j].getValue()==0) {
+						continue;
+					} else {
+						if((tiles[k-1][j].getValue() == 0)) {
+							tiles[k-1][j].setValue(val);
+							tiles[k][j].setValue(0);
+							valid ++;
+						} else if (tiles[k-1][j].getValue() == val) {
+							if(tiles[k-1][j].mergeable() && tiles[k][j].mergeable) {
+								tiles[k-1][j].setValue(val*2);
+								score += val*2;
+								tiles[k-1][j].Merged();
+								tiles[k][j].setValue(0);
+								valid ++;
+							}	
+						}
+					}
+				}
+			}
+		}
+		if(valid > 0) {
+			addRandomTile();
+			System.out.println("Valid Move");
+			moves++;
+		}else {
+			System.out.println("Invalid Move");
+			movesU = false;
+		}
 		
+		FindbiggestTile();
 	}
 	
 	public void moveDown() {
-		
+		int valid = 0;
+			for(int j = 0; j < 4; j++) {
+				for (int i=2; i >= 0; i--) {
+					for(int k = i; k <3; k++) {
+						int val = tiles[k][j].getValue();
+						if(tiles[k][j].getValue()==0) {
+							continue;
+						} else {
+							if((tiles[k+1][j].getValue() == 0)) {
+								tiles[k+1][j].setValue(val);
+								tiles[k][j].setValue(0);
+								valid ++;
+							} else if (tiles[k+1][j].getValue() == val) {
+								if(tiles[k+1][j].mergeable() && tiles[k][j].mergeable()) {
+									tiles[k+1][j].setValue(val*2);
+									score += val*2;
+									tiles[k+1][j].Merged();
+									tiles[k][j].setValue(0);
+									valid++;
+								}
+									
+							}
+						}
+						
+					}
+				}
+			}
+			if(valid > 0) {
+				addRandomTile();
+				System.out.println("Valid Move");
+				moves++;
+			}else {
+				System.out.println("Invalid Move");
+				movesD = false;
+			}
+		FindbiggestTile();
 	}
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		if(e.getSource() == start) {
-			start();
-		}
-		
-	}
-	
 	
 }
--- src/Index.java
+++ src/Index.java
@@ -1,9 +1,12 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.Box;
 import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -12,17 +15,27 @@ public class Index {
 	
 	public static void main (String[] args) {
 		Game newGame = new Game();
+//		start = new JButton("START");
+//		start.setBounds(342, 100, 80, 30);
+//		start.setBackground(backgroundColor);
+//		start.setFocusPainted(false);
+//		this.add(start);
+//		start.addActionListener(new ActionListener(){
+//
+//			@Override
+//			public void actionPerformed(ActionEvent e) {
+//				start();
+//			}
+//			
+//		});
 		
 		JFrame frame = new JFrame("2048");
 		frame.setSize(470, 640);
 		frame.setResizable(false);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
 		frame.setLayout(new BorderLayout());
 		frame.add(newGame, BorderLayout.CENTER);
-		
 		frame.setVisible(true);
-
 
 	}
 	
